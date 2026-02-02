9004346d39613cbe99c95971d83743abadb295d4
Sekine <tomatotamada@users.noreply.github.com>
2026-02-03 00:32:21 +0900

    fix static

--- Astro/astro-ioridram/astro.config.mjs
+++ Astro/astro-ioridram/astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import preact from '@astrojs/preact';
-import cloudflare from '@astrojs/cloudflare';
+import preact from "@astrojs/preact";
+import cloudflare from "@astrojs/cloudflare";
 
-import expressiveCode from 'astro-expressive-code';
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "static",
   adapter: cloudflare(),
-  integrations: [preact(), expressiveCode()]
+  integrations: [preact(), expressiveCode()],
 });
+
