5eb98e91719e2a08b0ae89967a48d7b527d1af81
iorimacbook <6774506+tomatotamada@users.noreply.github.com>
2026-04-09 22:54:59 +0900

    add post-8 & content.config.ts

---
+++ Astro/astro-ioridram/src/content.config.ts
@@ -0,0 +1,22 @@
+import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+
+const postCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/posts" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = {
+  posts: postCollection,
+};
